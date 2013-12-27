2093a6655b47051441ea4528a8805d30d9aead65
Zachary Kim <zachary.kim@gmail.com>
2013-12-27 01:28:28 -1000

    init mongo in worker proc

--- src/clj/inky/worker.clj
+++ src/clj/inky/worker.clj
@@ -1,12 +1,18 @@
 (ns inky.worker
   (:require [inky.compile :as comp]
-            [inky.env :as env]))
+            [inky.env :as env]
+            [somnium.congomongo :as mon]))
 
 (defn spawn-workers [num-workers]
   (doseq [id (repeatedly num-workers #(str (java.util.UUID/randomUUID)))]
     (println "Spawning worker" id)
     (future
-      (comp/run-worker! id))))
+      (try
+        (comp/run-worker! id)
+        (catch Exception e
+          (println e)
+          (.printStackTrace e))))))
 
 (defn -main []
+  (mon/set-connection! (mon/make-connection (env/str :mongo-url "mongodb://localhost:27017/inky")))
   (spawn-workers (env/int :num-workers 2)))
