f6d395ba7e359253200bb38973328cc33456c339
Zachary Kim <zachary.kim@gmail.com>
2014-01-14 23:21:11 -0800

    Bump to 0.1.5

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,6 +1,6 @@
-(defproject lein-inky "0.1.4"
+(defproject lein-inky "0.1.5"
   :description "Inky local sketch compiler"
   :url "http://github.com/zk/inky"
-  :dependencies [[inky "0.1.4"]]
+  :dependencies [[inky "0.1.5"]]
   :jar-name "lein-inky.jar"
   :eval-in-leiningen true)
